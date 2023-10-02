5d0c550661acb0e4cd5f5d9ff614f3da8bee0b14
Sorokovsky <Sorokovskys@ukr.net>
2023-10-02 20:33:27 +0300

    2 task

--- Task2/Task2/Source.c
+++ Task2/Task2/Source.c
@@ -1,17 +1,55 @@
-
-#include <stdio.h> 
-#include <conio.h> 
-int main(void)
+#include <stdio.h>
+#include <conio.h>
+void countOfString()
 {
-	int c, i = 0;
-	printf("\n Enter symbol-by-symbol the offer \"Hello, world\", \n press twice Ctrl + Z and press Enter: \n");
-	printf("\t");
-	while ((c = getchar()) != EOF) {
-		printf("\t");
-		c = getchar();
-		++i;
-	}
-	printf("\n\t The number of characters:%d \n", i);
-	printf("\n Press any key:"); _getch();
-	return 0;
+    char input;
+    int count = 0;
+    printf_s("Enter the string \"Hello, world\": ");
+    while (1) {
+        input = getchar(); // Input the char
+        if (input == '\n')
+        {
+            break;
+        }
+        count++;
+    }
+    printf_s("Count of characters: %d\n", count); // Display the result
+}
+void diagonal()
+{
+    char input, output = "";
+    int count = 0;
+    printf_s("Enter the name and surname: ");
+    while (1) {
+        input = getchar(); // Input the char
+        if (input == '\n')
+        {
+            break;
+        }
+        count++;
+        output += ' ' * count + input + '\n';
+        for (int i = 0; i < count; i++)
+        {
+            printf_s(" ");
+        }
+        printf_s("%c\n", input);
+    }
+}
+void dublicate(int count)
+{
+    char symbol;
+    printf_s("Enter the char:");
+    symbol = getchar();
+    printf_s("\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf_s("%c", symbol);
+    }
+}
+int main() 
+{
+   countOfString();
+   diagonal();
+   dublicate(2);
+   return 0;
 }
